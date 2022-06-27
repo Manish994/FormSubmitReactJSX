631ca3d96951e8883721be42bd87646361bd7ef9
Manish994 <paulsimon227@gmail.com>
2022-06-27 22:26:55 +0545

    Components props means function (parameter), props are in key:value pair, destructering

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+//components
 import UsernameField from "./UsernameField";
-import PasswordField from "./PasswordField";
 import EmailField from "./EmailField";
+import PasswordField from "./PasswordField";
 
 export default function Form() {
+  //status
+  const [usernameStatus, setUsernameStatus] = useState("");
+  const [emailStatus, setEmailStatus] = useState("");
+  //i'm showing 4 different error msg in password so use object instead
+  const [passwordStatus, setPasswordStatus] = useState({});
+  const [confirmPasswordStatus, setConfirmPasswordStatus] = useState("");
   return (
     <form>
       <div className="form_header">
         <h2>Sign Up</h2>
       </div>
       <div className="form_body">
-        <UsernameField />
-        <EmailField />
-        <PasswordField />
+        <UsernameField status={usernameStatus} setStatus={setUsernameStatus} />
+        <EmailField status={emailStatus} setStatus={setEmailStatus} />
+        <PasswordField
+          status={passwordStatus}
+          setStatus={setPasswordStatus}
+          confirmStatus={confirmPasswordStatus}
+          setConfirmStatus={setConfirmPasswordStatus}
+        />
       </div>
       <div>
         <button className="btn">Submit</button>
--- src/components/UsernameField.jsx
+++ src/components/UsernameField.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default function UsernameField() {
-  return <div>UsernameField</div>;
+export default function UsernameField({ status, setStaus }) {
+  //destructuring {} => if not destructring user (props)
+  return <div>User Name</div>;
 }
