a5b08f4cef9b53739e434a9d5f447354a4a76290
Manish994 <paulsimon227@gmail.com>
2022-06-28 21:58:59 +0545

    Done ConfirmPassword...... Remaining Password Field

--- src/components/PasswordField.jsx
+++ src/components/PasswordField.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 
-export default function PasswordField() {
-  return <div>PasswordField</div>;
+//import utility function
+import { getClassName, getIcon } from "../utilis/utils.form";
+export default function PasswordField({
+  status,
+  setStatus,
+  confirmStatus,
+  setConfirmStatus,
+}) {
+  const password = useRef("");
+  const confirmPassword = useRef("");
+  return (
+    <>
+      <div className="form_input mt-3">
+        <label htmlFor="password" className="small">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          placeholder="Enter Password"
+        />
+      </div>
+      <div className="form_input mt-3">
+        <label htmlFor="confirm-password">Confirm Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Confirm Password"
+          name="confirm-password"
+        />
+        <div className={getClassName(confirmStatus)}>
+          {getIcon(confirmStatus)}
+          <span>Password match</span>
+        </div>
+      </div>
+    </>
+  );
 }
